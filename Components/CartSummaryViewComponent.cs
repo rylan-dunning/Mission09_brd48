e5b9809a40f030ae44b7342a1b4512177dd85ba5
rylan-dunning <brdunning14@gmail.com>
2023-03-15 10:34:00 -0600

    did the #notcoveredinvideos part

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_brd48.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_brd48.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+
+
+    }
+}
